a2421a28b5273bff020f232df2ae3aac1d830ece
Daniel Wilson <wilsondr16@gmail.com>
2015-12-20 15:38:42 -0600

    Added ball and paddle, cleaned up game scene

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+
+	//basically event listeners for the OnTrigger and OnCollision events
+	void OnTriggerEnter2D(Collider2D collider){
+		print ("trigger!");
+		levelManager.LoadLevel("Win");
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision ){
+		print("collision");
+	}
+}
