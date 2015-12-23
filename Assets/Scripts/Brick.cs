cb29f4ef9910e1ed911b2fa455480e2b36885513
Daniel Wilson <wilsondr16@gmail.com>
2015-12-22 22:33:34 -0600

    Added brick sprites

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class Brick : MonoBehaviour {
 
-	public int maxHits;
+	//public int maxHits;
+	public Sprite[] hitSprites;
 	
 	private LevelManager levelManager; 
 	private int timesHit = 0;
@@ -18,10 +19,31 @@ public class Brick : MonoBehaviour {
 	}
 	
 	void OnCollisionEnter2D(Collision2D collision ){
+		bool isBreakable =(this.tag == "Breakable");
+		if(isBreakable){
+			HandleDamage();
+		}
+	}
+	
+	void HandleDamage(){
 		timesHit++;
+		//always 1 more hit than the number of damage stages added
+		int maxHits = hitSprites.Length + 1;
 		if(timesHit >= maxHits){
 			Destroy(gameObject);
 		}
+		else{
+			UpdateSprite();
+		}
+	}
+	
+	void UpdateSprite(){
+		int spriteIndex = timesHit - 1;
+		if(hitSprites[spriteIndex]){
+			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		}
+
+	
 	}
 	
 	void SimulateWin(){
